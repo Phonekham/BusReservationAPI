e9e3796cb965fdeef7dbd4bff8ec4a4d4d9ceb45
Phonekham <phonekham.dev@gmail.com>
2021-03-04 20:26:28 +0700

    get booked seats

--- src/graphql/resolvers/Booking/bookingQuery.js
+++ src/graphql/resolvers/Booking/bookingQuery.js
@@ -1,10 +1,23 @@
-import DepartureTime from '../../../models/DepartureTime';
+import DepartureTime from "../../../models/DepartureTime";
+import Seat from "../../../models/Seat";
+import BookingItem from "../../../models/BookingItem";
 
 const checkDepartureTime = async (parents, args, context, info) => {
   const { route } = args;
   return await DepartureTime.find({ route })
-    .populate({ path: 'busType' })
-    .populate({ path: 'route' });
+    .populate({ path: "busType" })
+    .populate({ path: "route" });
 };
 
-export default { checkDepartureTime };
+const getBookedSeats = async (parents, args, context, info) => {
+  const { departureDate, departureTime } = args;
+  const bookedSeats = await BookingItem.find({
+    departureDate: { $eq: departureDate },
+    departureTime: { $eq: departureTime },
+  });
+  const seatArr = bookedSeats.map((b) => b.seat);
+  const seats = await Seat.find({ _id: { $in: seatArr } });
+  return seats;
+};
+
+export default { checkDepartureTime, getBookedSeats };
