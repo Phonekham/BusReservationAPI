82c4f765c9e3baccbf9c94d9aa12573e73789b16
Phonekham <phone.madridista@gmail.com>
2021-03-10 18:39:15 +0700

    Update query.js

--- src/graphql/resolvers/Bus/query.js
+++ src/graphql/resolvers/Bus/query.js
@@ -1,34 +1,34 @@
-import { AuthenticationError } from 'apollo-server-express';
+import { AuthenticationError } from "apollo-server-express";
 
-import Bus from '../../../models/Bus';
+import Bus from "../../../models/Bus";
 
 export default {
   buses: async (parents, args, { user }, info) => {
     //   Check Auth
     if (!user) {
-      throw new AuthenticationError('Access denied', {
+      throw new AuthenticationError("Access denied", {
         errors: {
-          auth: 'ທ່ານບໍມີສິດ ກາລຸນາເຂົ້າສູ່ລະບົບຜູ້ດູແລລະບົບ',
+          auth: "ທ່ານບໍມີສິດ ກາລຸນາເຂົ້າສູ່ລະບົບຜູ້ດູແລລະບົບ",
         },
       });
     }
     return await Bus.find({})
-      .populate({ path: 'busType' })
-      .populate({ path: 'company' });
+      .populate({ path: "busType" })
+      .populate({ path: "company" });
   },
   bus: async (parents, args, { user }, info) => {
     //   Check Auth
     if (!user)
-      throw new Error('ທ່ານບໍມີສິດໃຊ້ງານຟັງຊັນນີ້ ກະລຸນາເຂົ້າສູ່ລະບົບ');
+      throw new Error("ທ່ານບໍມີສິດໃຊ້ງານຟັງຊັນນີ້ ກະລຸນາເຂົ້າສູ່ລະບົບ");
 
     try {
       return await Bus.findById(args.id)
-        .populate({ path: 'busType' })
-        .populate({ path: 'company' });
+        .populate({ path: "busType" })
+        .populate({ path: "company" });
     } catch (error) {
-      throw new Error('ເກີດຂໍ້ຜິດພາດ ບໍພົບໄອດີລົດທີ່ທ່ານຄົ້ນຫາ', {
+      throw new Error("ເກີດຂໍ້ຜິດພາດ ບໍພົບໄອດີລົດທີ່ທ່ານຄົ້ນຫາ", {
         errors: {
-          bus: 'ເກີດຂໍ້ຜິດພາດ ບໍພົບໄອດີລົດທີ່ທ່ານຄົ້ນຫາ',
+          bus: "ເກີດຂໍ້ຜິດພາດ ບໍພົບໄອດີລົດທີ່ທ່ານຄົ້ນຫາ",
         },
       });
     }
