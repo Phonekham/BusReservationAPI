c890650fb25a49a59cd95790c23c83ea49dc1536
Phonekham <phone.madridista@gmail.com>
2021-06-14 20:44:14 +0700

    remove seat query auth

--- src/graphql/resolvers/Seat/seatQuery.js
+++ src/graphql/resolvers/Seat/seatQuery.js
@@ -1,33 +1,27 @@
-import { AuthenticationError, UserInputError } from 'apollo-server-express';
+import { AuthenticationError, UserInputError } from "apollo-server-express";
 
-import Seat from '../../../models/Seat';
+import Seat from "../../../models/Seat";
 
 export default {
   seats: async (parents, args, { user }, info) => {
     const { busType } = args;
-    //   Check Auth
-    if (!user) {
-      throw new AuthenticationError(
-        'ທ່ານບໍມີສິດ ກາລຸນາເຂົ້າສູ່ລະບົບຜູ້ດູແລລະບົບ'
-      );
-    }
     return await Seat.find({ busType: { $eq: busType } }).populate({
-      path: 'busType',
+      path: "busType",
     });
   },
   seat: async (parents, args, { user }, info) => {
     //   Check Auth
     if (!user) {
       throw new AuthenticationError(
-        'ທ່ານບໍມີສິດ ກາລຸນາເຂົ້າສູ່ລະບົບຜູ້ດູແລລະບົບ'
+        "ທ່ານບໍມີສິດ ກາລຸນາເຂົ້າສູ່ລະບົບຜູ້ດູແລລະບົບ"
       );
     }
 
-    const seat = await Seat.findById(args.id).populate({ path: 'busType' });
+    const seat = await Seat.findById(args.id).populate({ path: "busType" });
     if (!seat) {
-      throw new UserInputError('Route ID Not Found', {
+      throw new UserInputError("Route ID Not Found", {
         errors: {
-          seat: 'ເກີດຂໍ້ຜິດພາດ ບໍພົບໄອດີບ່ອນນັ່ງນີ້',
+          seat: "ເກີດຂໍ້ຜິດພາດ ບໍພົບໄອດີບ່ອນນັ່ງນີ້",
         },
       });
     }
